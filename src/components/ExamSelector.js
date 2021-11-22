255120c77a2061eccd8a380a504a3c487931099e
Tyler D <drelyt@gmail.com>
2021-11-22 14:05:23 -0500

    Added exam descriptions to ExamSelector

--- src/components/ExamSelector.js
+++ src/components/ExamSelector.js
@@ -30,8 +30,11 @@ export default function ExamSelector({ onExamChange }) {
       <ExamChoice>
         <ExamTitle>Technician</ExamTitle>
         <ExamDescription>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto
-          fuga numquam eius dolore maiores delectus.
+          The entry-level license, known as <strong>Technician Class</strong>,
+          is awarded after an applicant successfully completes a 35-question
+          multiple choice written examination. The license grants full operating
+          privileges on all amateur bands above 30 MHz and limited privileges in
+          portions of the high frequency (HF) bands.
         </ExamDescription>
         <Button size="lg" onClick={() => onExamChange(ExamTypes.TECHNICIAN)}>
           Technician
@@ -41,10 +44,18 @@ export default function ExamSelector({ onExamChange }) {
       <ExamChoice>
         <ExamTitle>General</ExamTitle>
         <ExamDescription>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto
-          fuga numquam eius dolore.
+          The middle level, known as <strong>General Class</strong>, requires
+          passage of the Technician test, as well as a 35-question
+          multiple-choice General exam. General class licensees are granted
+          privileges on portions of all amateur bands, and have access to over
+          83% of all amateur HF bandwidth. However some band segments often used
+          for long distance contacts are not included.
         </ExamDescription>
-        <Button size="lg" onClick={() => onExamChange(ExamTypes.GENERAL)} disabled>
+        <Button
+          size="lg"
+          onClick={() => onExamChange(ExamTypes.GENERAL)}
+          disabled
+        >
           General
         </Button>
       </ExamChoice>
@@ -52,11 +63,16 @@ export default function ExamSelector({ onExamChange }) {
       <ExamChoice>
         <ExamTitle>Extra</ExamTitle>
         <ExamDescription>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto
-          fuga numquam eius dolore maiores delectus, laudantium nam animi
-          tempora minima omnis adipisci recusandae est iure.
+          The top US license class is <strong>Amateur Extra Class</strong>. This
+          license requires the same tests as General plus a 50-question
+          multiple-choice theory exam. Those with Amateur Extra licenses are
+          granted all privileges on all US amateur bands.
         </ExamDescription>
-        <Button size="lg" onClick={() => onExamChange(ExamTypes.EXTRA)} disabled>
+        <Button
+          size="lg"
+          onClick={() => onExamChange(ExamTypes.EXTRA)}
+          disabled
+        >
           Extra
         </Button>
       </ExamChoice>
